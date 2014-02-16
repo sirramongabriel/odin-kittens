1ad77096b08be9b2b3c2e61e676e7343f0445b9f
sirramongabriel <sirramongabriel@gmail.com>
2014-02-16 12:45:00 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OdinKittens::Application.config.secret_token = '08b482684ee254ae63e81044ea555a780a6c03f3f41889a44ae85e339eb07e0051eaae60a281a841d59d01e3915a4a21e835ef51e8e62a55839f94a55919be85'
